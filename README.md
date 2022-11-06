b9adbb5556792db3d524bf451bffc0ee310def73
Ben Song <ben.song@samarkand.global>
2022-11-06 10:45:55 +0800

    test

--- README.md
+++ README.md
@@ -1,1 +1,21 @@
 # operation_helper
+1. 验证码
+接收验证码并保存
+2. 自动化任务
+
+
+# guide
+## Development
+Bring up app-database cluster:
+```bash
+docker-compose -f docker-compose.app.yml up
+```
+
+## PGAdmin
+### Access to PgAdmin: 
+* **URL:** [http://localhost:5050](http://localhost:5050)
+* **Username:** user@domain.com (as a default)
+* **Password:** SuperSecret (as a default)
+
+### Add a new server in PgAdmin:
+* **PostgresHost** as `POSTGRES_HOST`, by default: `kong-database`
---
+++ server/db/postgres/tables.sql
@@ -0,0 +1,28 @@
+ create table "app_authentication" (
+    "app_authentication_id" serial primary key,
+
+    "type" varchar(100),
+    "platform" varchar,
+    "token" varchar,
+    "is_enabled" boolean default true,
+    
+    "create_time" timestamp(3) with time zone default current_timestamp,
+    "modify_time" timestamp(3) with time zone default current_timestamp,
+    "create_by" varchar(100),
+    "modify_by" varchar(100)
+);
+comment on column app_authentication.type is 'APP authentication type: sms, code';
+
+create table "app_operation_info" (
+    "app_operation_info_id" serial primary key,
+
+    "name" varchar(100),
+    "description" varchar(100),
+    "content" varchar,
+    "is_consumed" boolean default true,
+
+    "create_time" timestamp(3) with time zone default current_timestamp,
+    "modify_time" timestamp(3) with time zone default current_timestamp,
+    "create_by" varchar(100),
+    "modify_by" varchar(100)
+);
